117a42ad46aecb81388446e42f981ab07c199bb2
BearBiscuit <742035327@qq.com>
2023-06-07 14:51:16 +0800

    [add] add env path

--- src/train/sgnn/train.py
+++ src/train/sgnn/train.py
@@ -1,6 +1,11 @@
 import numpy as np
 import time
 import mmap
+import sys
+import os
+current_folder = os.path.abspath(os.path.dirname(__file__))
+#sys.path.append(current_folder+"/../"+"utils")
+
 
 def mergeFeat(file_hand,sampleNodes,featLen):
     feats = np.zeros((len(sampleNodes),featLen),dtype=np.int32)
@@ -12,29 +17,18 @@ def mergeFeat(file_hand,sampleNodes,featLen):
     print("mmap time :", time.time()-start)
     return feats
 
+def generate_random_sequence(min_val, max_val):
+    sequence = np.arange(min_val, max_val)
+    np.random.shuffle(sequence)
+    return sequence
+
+
+def env_init():
+    pass
+
+def main():
+    pass
+
 if __name__ == "__main__":
-    file_path = "./data/copy_1.bin"
-    file = open(file_path, "r+b")
-    mmapped_file = mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ)   
-    featLen = 16
-    feat1 = mergeFeat(mmapped_file,[1,2],featLen)
-
-    file_path2 = "./data/copy_2.bin"
-    file2 = open(file_path2, "r+b")
-    mmapped_file2 = mmap.mmap(file2.fileno(), 0, access=mmap.ACCESS_READ)   
-    feat2 = mergeFeat(mmapped_file2,[1,2],featLen)
-
-    file_path3 = "./data/copy_3.bin"
-    file3 = open(file_path3, "r+b")
-    mmapped_file3 = mmap.mmap(file3.fileno(), 0, access=mmap.ACCESS_READ)   
-    feat3 = mergeFeat(mmapped_file3,[1,2],featLen)
-    print(feat1)
-    print(feat2)
-    print(feat3)
-
-    mmapped_file.close()
-    mmapped_file2.close()
-    mmapped_file3.close()
-    file.close()
-    file2.close()
-    file3.close()
+    main()
+    print(current_folder)
